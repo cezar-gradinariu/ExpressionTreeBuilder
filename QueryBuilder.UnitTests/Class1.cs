622d752a952e2eed7a1465cd86572f60794112ba
Cezar.Gradinariu <cezar.gradinariu@live.com>
2015-07-26 22:42:35 +1000

    nunits

---
+++ QueryBuilder.UnitTests/Class1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace QueryBuilder.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        private readonly QueryObject _qObject = new QueryObject
+        {
+            QueryConditions = new List<QueryObject.QueryCondition>
+            {
+                new QueryObject.QueryCondition
+                {
+                    PropertyName = "Name",
+                    Operation = "Contains",
+                    Value = "na"
+                },
+                new QueryObject.QueryCondition
+                {
+                    PropertyName = "Surname",
+                    Operation = "Equal",
+                    Value = "Gradinariu"
+                },
+                new QueryObject.QueryCondition
+                {
+                    PropertyName = "Age",
+                    Operation = "LessThanOrEqual",
+                    Value = 10
+                }
+            },
+            GridCriteria = new GridCriteria
+            {
+                Pagination = new PaginationCriteria
+                {
+                    PageSize = 1,
+                    StartIndex = 1
+                },
+                SortCriteria = new List<SortCriteria>
+                {
+                    new SortCriteria("Age", SortOrder.Descendant),
+                    new SortCriteria("Name", SortOrder.Ascendant),
+                    new SortCriteria("Surname", SortOrder.Ascendant)
+                }
+            }
+        };
+
+        private readonly List<Person> _list = new List<Person>
+        {
+            new Person {Name = "B", Surname = "G", Age = 30},
+            new Person {Name = "A", Surname = "G1", Age = 35},
+            new Person {Name = "Ana-Maria", Surname = "Gradinariu", Age = 3},
+            new Person {Name = "Maya-Ioana", Surname = "Gradinariu", Age = 1},
+            new Person {Name = "B", Surname = "G", Age = 30},
+            new Person {Name = "A", Surname = "G1", Age = 35},
+            new Person {Name = "Ana-Maria", Surname = "Gradinariu", Age = 3},
+            new Person {Name = "Maya-Ioana", Surname = "Gradinariu", Age = 1},
+        };
+
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Surname { get; set; }
+        }
+
+        [Test]
+        public void PerformTimeTests()
+        {
+            const int noOfCalls = 1000;
+            var watch = new Stopwatch();
+            watch.Start();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    _list.Where(p => p.Name.Contains("na") && p.Surname == "Gradinariu" && p.Age <= 10)
+                        .OrderByDescending(p => p.Age)
+                        .ThenBy(p => p.Name)
+                        .ThenBy(p => p.Surname)
+                        .Take(1)
+                        .Skip(0)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Direct lambda for {0} calls took {1}ms.", noOfCalls, watch.ElapsedMilliseconds);
+
+            watch.Restart();
+            var ex = _qObject.BuildExpression<Person>().Compile();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    _list.AsQueryable().Where(ex).AsQueryable().GetSortedPageAsList(_qObject.GridCriteria).ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call using cached 'where expression' for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+
+            watch.Restart();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    _list.AsQueryable()
+                        .Where(_qObject.BuildExpression<Person>().Compile())
+                        .AsQueryable()
+                        .GetSortedPageAsList(_qObject.GridCriteria)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call with 'where expression' compiled explicitly for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+
+
+            watch.Restart();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    _list.AsQueryable()
+                        .Where(_qObject.BuildExpression<Person>())
+                        .AsQueryable()
+                        .GetSortedPageAsList(_qObject.GridCriteria)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call with 'where expression' not compiled for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ QueryBuilder.UnitTests/Class2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace QueryBuilder.UnitTests
+{
+    [TestFixture]
+    public class Class2
+    {
+        private static readonly List<Company> List = new List<Company>
+        {
+            new Company("c", 3),
+            new Company("c", 2),
+            new Company("c", 1),
+            new Company("a", 1),
+            new Company("a", 2),
+            new Company("a", 3),
+            new Company("b", 3),
+        };
+
+        private readonly QueryObject _qObject = new QueryObject
+        {
+            QueryConditions = new List<QueryObject.QueryCondition>
+            {
+                new QueryObject.QueryCondition
+                {
+                    PropertyName = "Name",
+                    Operation = "NotEqual",
+                    Value = "b"
+                }
+            },
+            GridCriteria = new GridCriteria
+            {
+                Pagination = new PaginationCriteria
+                {
+                    PageSize = 1,
+                    StartIndex = 1
+                },
+                SortCriteria = new List<SortCriteria>
+                {
+                    new SortCriteria("Name", SortOrder.Ascendant),
+                    new SortCriteria("Stars", SortOrder.Ascendant)
+                }
+            }
+        };
+        public class Company
+        {
+            public Company(string name, int stars)
+            {
+                Name = name;
+                Stars = stars;
+            }
+
+            public string Name { get; set; }
+            public int Stars { get; set; }
+
+            public override string ToString()
+            {
+                return Name + " " + Stars;
+            }
+        }
+
+        [Test]
+        public void PerformTimeTests()
+        {
+            const int noOfCalls = 1000;
+            var watch = new Stopwatch();
+            watch.Start();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    List.Where(p => p.Name != "b")
+                        .OrderBy(p => p.Name)
+                        .ThenBy(p => p.Stars)
+                        .Take(1)
+                        .Skip(0)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Direct lambda for {0} calls took {1}ms.", noOfCalls, watch.ElapsedMilliseconds);
+
+            watch.Restart();
+            var ex = _qObject.BuildExpression<Company>().Compile();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    List.AsQueryable().Where(ex).AsQueryable().GetSortedPageAsList(_qObject.GridCriteria).ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call using cached 'where expression' for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+
+            watch.Restart();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    List.AsQueryable()
+                        .Where(_qObject.BuildExpression<Company>().Compile())
+                        .AsQueryable()
+                        .GetSortedPageAsList(_qObject.GridCriteria)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call with 'where expression' compiled explicitly for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+
+
+            watch.Restart();
+            for (var i = 0; i < noOfCalls; i++)
+            {
+                var s =
+                    List.AsQueryable()
+                        .Where(_qObject.BuildExpression<Company>())
+                        .AsQueryable()
+                        .GetSortedPageAsList(_qObject.GridCriteria)
+                        .ToList();
+            }
+            watch.Stop();
+            Console.WriteLine("Call with 'where expression' not compiled for {0} calls took {1}ms.", noOfCalls,
+                watch.ElapsedMilliseconds);
+        }
+    }
+}
